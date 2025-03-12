36b66a3efa22ed6208c0550e54bc5d9319acd765
sharayuaswale-23 <sharayuaswale123@gmail.com>
2025-03-12 11:18:22 +0530

    second commit

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -2,19 +2,19 @@ import Slider from "./slider";
 
 export default function HeroSection() {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-center md:space-x-8 px-4 py-16">
+    <div className="flex flex-col md:flex-row items-center justify-center md:space-x-8 min-h-screen">
 
-    <section className="w-full md:w-1/2 text-center md:text-left">
+    <section className="w-full md:w-1/2  mx-12 my-12 text-center md:text-left">
         <div className="max-w-2xl mx-auto">
             <h2 className="text-3xl md:text-5xl font-bold text-gray-900">One stop solution <span className="text-pink-500">E-Store</span></h2>
             <p className="mt-4 text-gray-600">Discover the latest headphones, earphones, mobiles, tablets etc.</p>
             <p className="text-gray-600">Exclusive deals just for you!</p>
-            <a href="#" className="mt-6 inline-block bg-gray-200 text-pink-500 px-6 py-3 rounded-lg text-lg shadow-md hover:bg-gray-600 transition">Shop Now</a>
+            <a href="/store" className="mt-6 inline-block bg-gray-200 text-pink-500 px-6 py-3 rounded-lg text-lg shadow-md hover:bg-gray-600 transition">Shop Now</a>
         </div>
     </section>
     
  
-    <section className="w-full md:w-1/2">
+    <section className="w-full h-full md:w-1/2">
         <Slider/>
     </section>
 </div>
--- src/components/home/slider.jsx
+++ src/components/home/slider.jsx
@@ -1,9 +1,36 @@
-"use state"
+"use client";
+import { useState, useEffect } from "react";
+
+const images = [
+  "https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1692947383286-714WUJlhbLS._SL1500_.jpg",
+  "https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1692255251854-xbox.jpg",
+  "https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1692257709689-logitech heaphone.jpg",
+  "https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1692941008275-headphone3.jpg",
+];
 
 export default function Slider() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <section className="bg-gray-800 text-white p-4">
-      <h1 className="text-2xl">Slider</h1>
-    </section>
+    <div className="relative w-full h-[350px] md:h-[450px] overflow-hidden">
+      {images.map((img, index) => (
+        <img
+          key={index}
+          src={img}
+          alt={`Slide ${index + 1}`}
+          className={`absolute inset-0 w-full h-full object-cover transition-opacity duration-1000 ${
+            index === currentIndex ? "opacity-100" : "opacity-0"
+          }`}
+        />
+      ))}
+    </div>
   );
 }
